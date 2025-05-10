990bd026650aa23592689e73307181634db39793
Raksa Lim <raksalim97@gmail.com>
2025-05-10 15:59:27 -0400

    new headshot background and bio

--- src/pages/underConstruction/UnderConstruction.tsx
+++ src/pages/underConstruction/UnderConstruction.tsx
@@ -1,5 +1,6 @@
-import backgroundImage from '../../assets/EmilyWithBellaWebsite.jpg';
-import headshot from '../../assets/headshot.jpg';
+import backgroundImage from '../../assets/bella-on-chair.jpg';
+import headshot from '../../assets/headshot-1.jpg';
+import './UnderConstruction.css'
 
 export function UnderConstructionPage() {
 
@@ -25,7 +26,8 @@ export function UnderConstructionPage() {
                     }         
                     @media (max-width: 768px) {
                         .mobile-font {
-                            font-size: 20px !important;
+                            font-size: 12px !important;
+                            padding: 26vh 20px !important;
                         }
                     }
                      
@@ -42,36 +44,20 @@ export function UnderConstructionPage() {
                         position: 'absolute',
                         top: 0,
                         left: 0,
-                        height: '50%',
+                        height: '80%',
                         width: '100%',
                         backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                        animation: 'slideDown 2s ease-in-out'
+                        boxShadow: '0px 10px 10px rgba(0, 0, 0, .5)',
+                        animation: 'slideDown 2s ease-in-out',
+                        display: 'flex',
+                        justifyContent: 'center',
                     }}
                 >
                     <div
                         style={{
-                            position: 'absolute',
-                            top: '50%',
-                            left: '50%',
-                            transform: 'translate(-50%, -50%)',
-                            color: 'white',
-                            textAlign: 'center',
-                            fontSize: '2rem',
-                            fontWeight: 'bold',
-                        }}
-                    >
-                        <style>
-
-                        </style>
-                        <span className="mobile-font">
-                            Emily's Website is Coming Soon!
-                        </span>
-                    </div>
-                    <div
-                        style={{
                             animation: 'fadeIn 1.5s ease-in-out 0.5s forwards', // 2s delay to match the slideDown animation
                             position: 'absolute',
-                            top: '80%',
+                            top: '15%',
                             left: '50%',
                             transform: 'translateX(-50%)',
                             width: '150px',
@@ -92,6 +78,42 @@ export function UnderConstructionPage() {
                             }}
                         />
                     </div>
+                    <div
+                        className='mobile-font'
+                        style={{
+                            marginTop: '20px',
+                            fontSize: '1.2rem',
+                            lineHeight: '1.5',
+                            color: 'white',
+                            maxWidth: '600px',
+                            textAlign: 'center',
+                            padding: '24vh 20px',
+                        }}
+                    >
+                        <h2 style={{
+                            fontSize: '2rem',
+                            marginBottom: '10px',
+                            animation: 'fadeIn 1.5s ease-in-out forwards',
+                        }}>
+                            Emily Lim
+                        </h2>
+                        <p>
+                            is a software engineer and writer based in Virginia,
+                            <br />represented by Nalini Akolekar of <br />
+                            <a href='https://www.spencerhillassociates.com/'>Spencerhill Associates</a>.
+                        </p>
+                        <p>
+                            Her short fiction has been published in Sweet Tree Review, Bartleby, and Corvid Queen, and her latest novel, MERGE CONFLICT, was selected for the 2024 SmoochPit Mentorship Program.
+                            <br/>When not staring at a laptop screen, she enjoys gardening, running Dungeons & Dragons campaigns, and attempting overly ambitious sewing projects.
+                        </p>
+                        <p>
+                            Website under construction.
+                        </p>
+                        <p>
+                            We have our best cats on the case.
+                        </p>
+                    </div>
+
                 </div>
 
                 <div
